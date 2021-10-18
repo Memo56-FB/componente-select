fb642dd0181cc7ff0a41dd24510cd8793afb0e38
Guillermo Farfan <memo56_fb@hotmail.com>
2021-10-18 13:46:23 -0500

    mejorar validación

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 (function() {
-    const selectBranchesMazda_filter = document.querySelectorAll('.branches-mazda-form__select-branches');
-    const optionsMazda_filter = Array.from(selectBranchesMazda_filter[0].options);
     const inputFilterMazda_filter = document.querySelector('.branches-mazda-form__input-filter');
     if(inputFilterMazda_filter){
+    const selectBranchesMazda_filter = document.querySelectorAll('.branches-mazda-form__select-branches');
+    const optionsMazda_filter = Array.from(selectBranchesMazda_filter[0].options);
+    
         function findBranches (search, optionsMazda_filter) {
         return optionsMazda_filter.filter(option => {
             const regex = new RegExp(search, 'gi');
