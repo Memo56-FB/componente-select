d436aacc476ce8574d981acbd957d066b7339f8d
Guillermo Farfan <memo56_fb@hotmail.com>
2021-10-18 10:51:38 -0500

    agregar IIFE a la función

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const selectBranchesMazda_filter = document.querySelectorAll('.branches-mazda-form__select-branches');
-const optionsMazda_filter = Array.from(selectBranchesMazda_filter[0].options);
-const inputFilterMazda_filter = document.querySelector('.branches-mazda-form__input-filter');
-
-function findBranches (search, optionsMazda_filter) {
-return optionsMazda_filter.filter(option => {
-    const regex = new RegExp(search, 'gi');
-    return option.text.match(regex);
-});
-}
-
-function filterBranches () {
-optionsMazda_filter.forEach(option => { 
-    option.remove();
-    option.selected = false;
-});
-const matchArray = findBranches(this.value, optionsMazda_filter);
-selectBranchesMazda_filter[0].append(...matchArray);
-}
-
-
-inputFilterMazda_filter.addEventListener('change', filterBranches);
-inputFilterMazda_filter.addEventListener('keyup', filterBranches);
+(function() {
+    const selectBranchesMazda_filter = document.querySelectorAll('.branches-mazda-form__select-branches');
+    const optionsMazda_filter = Array.from(selectBranchesMazda_filter[0].options);
+    const inputFilterMazda_filter = document.querySelector('.branches-mazda-form__input-filter');
+    
+    function findBranches (search, optionsMazda_filter) {
+    return optionsMazda_filter.filter(option => {
+        const regex = new RegExp(search, 'gi');
+        return option.text.match(regex);
+    });
+    }
+    
+    function filterBranches () {
+    optionsMazda_filter.forEach(option => { 
+        option.remove();
+        option.selected = false;
+    });
+    const matchArray = findBranches(this.value, optionsMazda_filter);
+    selectBranchesMazda_filter[0].append(...matchArray);
+    }
+    
+    
+    inputFilterMazda_filter.addEventListener('change', filterBranches);
+    inputFilterMazda_filter.addEventListener('keyup', filterBranches);
+}) ()
